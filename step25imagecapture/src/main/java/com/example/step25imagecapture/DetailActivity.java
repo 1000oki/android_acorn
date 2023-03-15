9a821b32d16b7432b7b72c28f3e6fc1ebeedaff2
OkHui <100889562+1000oki@users.noreply.github.com>
2023-03-15 11:21:45 +0900

    step25 imagecapture MyHttpUtil 완성하고 사용하는 구조로 변경중...

--- step25imagecapture/src/main/java/com/example/step25imagecapture/DetailActivity.java
+++ step25imagecapture/src/main/java/com/example/step25imagecapture/DetailActivity.java
@@ -1,29 +1,36 @@
 package com.example.step25imagecapture;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.bumptech.glide.Glide;
 import com.example.step25imagecapture.databinding.ActivityDetailBinding;
+import com.example.step25imagecapture.util.MyHttpUtil;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements MyHttpUtil.RequestListener {
 
     ActivityDetailBinding binding;
     SharedPreferences pref;
@@ -57,27 +64,37 @@ public class DetailActivity extends AppCompatActivity {
         pref=PreferenceManager.getDefaultSharedPreferences(this);
         sessionId=pref.getString("sessionId", "");
 
-        // 삭제 버튼에 리스너 등록
+        //삭제 버튼에 리스너 등록
         binding.deleteBtn.setOnClickListener(v -> {
             new AlertDialog.Builder(this)
-                    .setMessage("삭제하시겠습니까?")
+                    .setMessage("삭제 하시겠습니까?")
                     .setPositiveButton("네", (dialog, which) -> {
-                        // 삭제할 갤러리 사진의 Primary Key를 이용해서 삭제 작업을 진행한다.
-                        int num = dto.getNum();
+                        //삭제할 갤러리 사진의 Primary Key 를 이용해서 삭제 작업을 진행한다.
+                        int num=dto.getNum();
+
                     })
-                    .setNeutralButton("아니요", null)
+                    .setNegativeButton("아니요", null)
                     .create()
                     .show();
         });
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        new LoginCheckTask().execute(AppConstants.BASE_URL+"/music/logincheck");
     }
+
+    @Override
+    public void onSuccess(int requestId, String data) {
+
+    }
+
+    @Override
+    public void onFail(int requestId, Map<String, Object> result) {
+
+    }
+
 
     //로그인 여부를 체크하는 작업을 할 비동기 task
     class LoginCheckTask extends AsyncTask<String, Void, String> {
@@ -184,3 +201,16 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
